87ccff10ed3d2c14fd6c01215524f1989f1fe569
Preston-Too <toopreston@gmail.com>
2021-01-09 10:31:21 +0300

    added view function search_results

--- photos/views.py
+++ photos/views.py
@@ -9,3 +9,16 @@ def photos(request):
 def locations(request,location):
     locations = Image.filterimageByLocation(location)
     return render(request,'locations.html',{'locations':locations})
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_photos = Image.searchImage(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})

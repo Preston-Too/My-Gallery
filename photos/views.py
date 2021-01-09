65b13b87702e21ba760aadc85e3c74b8b72d1e87
Preston-Too <toopreston@gmail.com>
2021-01-09 10:05:02 +0300

    added views function photos

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def photos(request):
+    photos = Image.objects.all()
+    locations = Location.objects.all()
+    return render(request,'photos.html',{'photos':photos,'locations':locations})

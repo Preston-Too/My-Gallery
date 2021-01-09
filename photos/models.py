e05dd9facb2290def70ffb6da8a53d67fd2451f0
Preston-Too <toopreston@gmail.com>
2021-01-09 23:44:13 +0300

    added class location

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class Location(models.Model):
+    locationName = models.CharField(max_length=30)
+
+    def saveLocation(self):
+        self.save()
+
+    def deleteLocation(self):
+        self.delete()
+
+    @classmethod
+    def updateLocation(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def __str__(self):
+        return self.locationName

56c86ff6072ecdbfb7b358ea84c930de2cb185d9
Preston-Too <toopreston@gmail.com>
2021-01-10 21:36:31 +0300

    modified settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'moringa',
+        'PASSWORD':'Access',
     }
 }
 
@@ -126,7 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 cloudinary.config( 
     cloud_name="dztu4rgdo",

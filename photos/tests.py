0212a81d456989ba74f77f98d6dd0223a8243a0d
Preston-Too <toopreston@gmail.com>
2021-01-11 08:07:33 +0300

    added setup to test image

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Image, Category, Location
 
 # Create your tests here.
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(locationName='Nairobi')
+        self.location.saveLocation()
+
+        self.category = Category(categoryName='nature')
+        self.category.saveCategory()
+
+        self.testInstance = Image(id=1, imageName='IMG__001', imageDescription=' a test image', imageLocation=self.location,
+                                imageCategory=self.category)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.testInstance, Image))

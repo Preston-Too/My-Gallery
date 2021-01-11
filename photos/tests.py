a2d0dab3559c9ba1afd63d25f0624626b98d294f
Preston-Too <toopreston@gmail.com>
2021-01-11 08:23:37 +0300

    added test for location

--- photos/tests.py
+++ photos/tests.py
@@ -51,3 +51,26 @@ class TestImage(TestCase):
         Image.objects.all().delete()
         Location.objects.all().delete()
         Category.objects.all().delete()
+
+class TestLocation(TestCase):
+    def setUp(self):
+        self.location = Location(name='Nairobi')
+        self.location.saveLocation()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_location(self):
+        self.location.saveLocation()
+        locations = Location.getLocations()
+        self.assertTrue(len(locations) > 0)
+
+    def test_get_locations(self):
+        self.location.saveLocation()
+        locations = Location.getLocations()
+        self.assertTrue(len(locations) > 1)
+
+    def test_delete_location(self):
+        self.location.deleteLocation()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
